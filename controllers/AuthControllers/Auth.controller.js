ef8093c9ef7882c452eca82e07d43c858bd66ba4
TNT-B <thanhbinh8223@gmail.com>
2023-03-23 18:53:14 +0700

    debug test

--- controllers/AuthControllers/Auth.controller.js
+++ controllers/AuthControllers/Auth.controller.js
@@ -1,45 +1,48 @@
-const AuthModels = require('../../models/users.schema');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const AuthModels = require("../../models/users.schema");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const JWT_SECRET_KEY = 'h3254hgu4123ug';
+// câu 4: Create a login API. Generate a token when user get login.
+const JWT_SECRET_KEY = "mysecretkey";
 
 const AuthHandler = (req, res) => {
-    const { username, password } = req.body;
-    AuthModels.findOne({
-        username
-    }).then((result)=>{
-        if(result){
-            if(password==result.password){
-                const newToken = jwt.sign({
-                    username: username,
-                    password: password
-                }, JWT_SECRET_KEY);
-                res.status(200).json({
-                    msg: "Login successfully",
-                    token: newToken
-                })
-            }
-            else{
-                res.status(403).json({
-                    msg: "Password incorrectly",
-                    token: null
-                })
-            }
+  const { username, password } = req.body;
+  AuthModels.findOne({
+    username,
+  })
+    .then((result) => {
+      if (result) {
+        if (password == result.password) {
+          const newToken = jwt.sign(
+            {
+              username: username,
+              password: password,
+            },
+            JWT_SECRET_KEY
+          );
+          res.status(200).json({
+            msg: "Login successfully",
+            token: newToken,
+          });
+        } else {
+          res.status(403).json({
+            msg: "Password incorrectly",
+            token: null,
+          });
         }
-        else{
-            res.status(403).json({
-                msg: "Can not find this account",
-                token: null
-            })
-        }
-    }).catch((err)=>{
+      } else {
         res.status(403).json({
-            msg: err,
-            token: null
-        })
-    })  
-}
-
+          msg: "Can not find this account",
+          token: null,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(403).json({
+        msg: err,
+        token: null,
+      });
+    });
+};
 
 module.exports = AuthHandler;
--- routes/Server.route.js
+++ routes/Server.route.js
@@ -1,27 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const AuthHandler = require('../controllers/AuthControllers/Auth.controller');
-const [getAllInventories, getOrders, getLowQuantityInventories] = require('../controllers/DataControllers/DataServer.controller');
-const jwt = require('jsonwebtoken');
-const JWT_SECRET_KEY = 'h3254hgu4123ug';
-const checkAuthenticated = (req, res, next)=>{
-    const { token } = req.body;
-    jwt.verify(token, JWT_SECRET_KEY, (err, encoded)=>{
-        if(!err){
-            next();
-        }
-        else{
-            res.status(403).json({
-                msg: "You must log in or sign up before!",
-                error: err
-            })
-        }
-    })
-}
+const AuthHandler = require("../controllers/AuthControllers/Auth.controller");
+const [
+  getAllInventories,
+  getOrders,
+  getLowQuantityInventories,
+] = require("../controllers/DataControllers/DataServer.controller");
 
-router.post('/login', AuthHandler);
-router.get('/inventories/all',checkAuthenticated, getAllInventories);
-router.get('/inventories/low',checkAuthenticated, getLowQuantityInventories);
-router.get('/orders', checkAuthenticated, getOrders);
+// câu 5 : Restrict the resource. Only logged-in user can visit it.
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = "mysecretkey";
+const checkAuthenticated = (req, res, next) => {
+  const { token } = req.body;
+  jwt.verify(token, JWT_SECRET_KEY, (err, encoded) => {
+    if (!err) {
+      next();
+    } else {
+      res.status(403).json({
+        msg: "You must log in or sign up before!",
+        error: err,
+      });
+    }
+  });
+};
+
+router.post("/login", AuthHandler);
+router.get("/inventories/all", checkAuthenticated, getAllInventories);
+router.get("/inventories/low", checkAuthenticated, getLowQuantityInventories);
+router.get("/orders", checkAuthenticated, getOrders);
 
 module.exports = router;
